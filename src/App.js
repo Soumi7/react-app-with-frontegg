9b6b79d5011919e61720cdc372804d9e295ae7a2
Soumi7 <soumibardhan10@gmail.com>
2021-12-22 13:46:11 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useAuth } from '@frontegg/react'
 
 function App() {
+  const { user, isAuthenticated } = useAuth();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {isAuthenticated && (
+        <div>
+          <img src={user.profilePictureUrl} alt={user.name} />
+          <span>{user.name}</span>
+        </div>
+      )}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
+
+import { FronteggProvider } from '@frontegg/react';
+
+const contextOptions = {
+  baseUrl: 'https://app-qmpl7qfdbthr.frontegg.com',
+};
 
 ReactDOM.render(
-  <React.StrictMode>
+  <FronteggProvider contextOptions={contextOptions}>
     <App />
-  </React.StrictMode>,
+  </FronteggProvider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
